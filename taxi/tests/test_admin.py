816c5365518346d3b140b4587e178c6cf9235b02
Nick Konkov <kharkivnick@gmail.com>
2025-11-16 00:39:03 +0200

    Solution

---
+++ taxi/tests/test_admin.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class TestAdmin(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            username="admin",
+            password="test123"
+        )
+        self.client.force_login(self.admin_user)
+        self.driver = get_user_model().objects.create_user(
+            username="driver",
+            password="test456",
+            license_number="TST12345",
+        )
+
+    def test_driver_license_listed(self):
+        """
+        test that driver's license is in list_display on driver admin page
+        """
+        url = reverse("admin:taxi_driver_changelist")
+        res = self.client.get(url)
+        self.assertContains(res, self.driver.license_number)
+
+    def test_driver_detail_license_listed(self):
+        """
+        test that driver's license is on driver detail admin page
+        """
+        url = reverse("admin:taxi_driver_change", args=[self.driver.id])
+        res = self.client.get(url)
+        self.assertContains(res, self.driver.license_number)
