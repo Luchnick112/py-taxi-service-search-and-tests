3002cd8ea10e3af621fe9d2fd200d4fb88ba1bd1
Nick Konkov <kharkivnick@gmail.com>
2025-11-16 01:58:16 +0200

    Solution2

--- taxi/tests/test_views.py
+++ taxi/tests/test_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from taxi.models import Manufacturer
+from taxi.models import Manufacturer, Car
 
 MANUFACTURER_URL = reverse("taxi:manufacturer-list")
 
@@ -57,3 +57,53 @@ class PrivateDriverTest(TestCase):
         self.assertEqual(new_user.first_name, form_data["first_name"])
         self.assertEqual(new_user.last_name, form_data["last_name"])
         self.assertEqual(new_user.license_number, form_data["license_number"])
+
+
+class DriverSearchTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="search_user", password="pass123"
+        )
+        self.client.force_login(self.user)
+        get_user_model().objects.create_user(
+            username="john_doe", password="pass", license_number="ABC12345"
+        )
+        get_user_model().objects.create_user(
+            username="jane_smith", password="pass", license_number="XYZ54321"
+        )
+
+    def test_search_driver_by_username(self):
+        response = self.client.get(reverse("taxi:driver-list"), {"username": "john"})
+        self.assertContains(response, "john_doe")
+        self.assertNotContains(response, "jane_smith")
+
+
+class CarSearchTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="search_user", password="pass123"
+        )
+        self.client.force_login(self.user)
+        manufacturer = Manufacturer.objects.create(name="Toyota", country="Japan")
+        Car.objects.create(model="Corolla", manufacturer=manufacturer)
+        Car.objects.create(model="Camry", manufacturer=manufacturer)
+
+    def test_search_car_by_model(self):
+        response = self.client.get(reverse("taxi:car-list"), {"model": "rolla"})
+        self.assertContains(response, "Corolla")
+        self.assertNotContains(response, "Camry")
+
+
+class ManufacturerSearchTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="search_user", password="pass123"
+        )
+        self.client.force_login(self.user)
+        Manufacturer.objects.create(name="BMW", country="Germany")
+        Manufacturer.objects.create(name="Audi", country="Germany")
+
+    def test_search_manufacturer_by_name(self):
+        response = self.client.get(reverse("taxi:manufacturer-list"), {"name": "bmw"})
+        self.assertContains(response, "BMW")
+        self.assertNotContains(response, "Audi")
